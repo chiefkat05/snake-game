52905b02118dd74e431fbc67c4e05b21d9e54ca6
Chiefkat05 <jamesville05@gmail.com>
2025-12-11 12:32:01 -0600

    05 - animation system implemented

--- main.c
+++ main.c
@@ -10,8 +10,148 @@
 
 // game plan
 
-// get current time from sdl and implement a platform-agnostic tick system
-// remake snake graphics for all the different cases of head/body/tail movement, and apply them in the code
+// apply non-animated snake graphics in the code
+// make animation system be apply-and-forget, by making an imageAnimate function where you specify the animation components (cont.)
+//      and the imageDraw function handles the animating sprite by checking if there's an animation in play and picking the appropriate (cont.)
+//      sprite to draw.
+
+/*
+
+    // behind the scenes: hold a specific animation pointer that is the "current edited animation"
+    // (note: the frameaction and framesound functions are theoretical, don't implement them today)
+    beginAnimation(&snakeAnim);
+    addFrame(0, 0, 100); // hold sprite texture position 0, 0 for 100 ticks
+    // addFrameAction((void *)funcFoo()); // hmm
+    addFrame(1, 0, 100);
+    // addFrameSound(&snakeHissSnd); // could also be done with the frameAction but I think having a dedicated function for this is worth (You'll probably be adding a bunch of sounds to animations)
+    // examples of animations that could have sounds play on a frame:
+    //      snake hisses on the frame he sticks his tongue out
+    //      player step sound
+    //      title card shine sound when the shine frames play
+    //      there's an endless selection, endless - brian regan
+    endAnimation(LOOPING); // apply final animation properties with a bitflag (like whether it loops) (in fact just make this the loopcheck rn)
+    // other animation flags in the future could be REVERSE (MIRRORED), PING-PONG, more if you think of them
+
+    imageDrawAnimated(&snakeImg, &snakeAnim);
+*/
+
+#define MAX_ANIMATION_FRAMES 16
+typedef struct _Animation
+{
+    int xframes[MAX_ANIMATION_FRAMES];
+    int yframes[MAX_ANIMATION_FRAMES];
+    int timeframes[MAX_ANIMATION_FRAMES];
+    unsigned int framecount;
+    
+    int time;
+    unsigned int frame;
+
+    uint8 args;
+
+    struct _Animation *next;
+} Animation;
+
+#define MAX_ANIMATIONS 128
+typedef struct
+{
+    Animation animations[MAX_ANIMATIONS];
+    unsigned int head;
+    Animation *free_animations;
+
+    Animation *editing_animation;
+} AnimationPool;
+
+AnimationPool anim_pool;
+
+Animation *AnimationAlloc()
+{
+    if (anim_pool.head != MAX_ANIMATIONS)
+    {
+        return &anim_pool.animations[anim_pool.head++];
+    }
+    if (anim_pool.free_animations)
+    {
+        Animation *out = anim_pool.free_animations;
+        anim_pool.free_animations = anim_pool.free_animations->next;
+        return out;
+    }
+
+    verify(false, "Ran out of animations, please tell chiefkat to increase the limit or implement a better animation system", __LINE__);
+    return NULL;
+}
+void beginAnimation(Animation *anim)
+{
+    anim_pool.editing_animation = anim;
+    anim_pool.editing_animation->framecount = 0;
+}
+void addFrame(int x, int y, int time)
+{
+    if (!anim_pool.editing_animation)
+    {
+        verify(false, "there is no animation to add frames to. Please specify the editing animation with beginAnimation()", __LINE__);
+    }
+    
+    unsigned int frame = anim_pool.editing_animation->framecount;
+    anim_pool.editing_animation->xframes[frame] = x;
+    anim_pool.editing_animation->yframes[frame] = y;
+    anim_pool.editing_animation->timeframes[frame] = time;
+    ++anim_pool.editing_animation->framecount;
+}
+void endAnimation(uint8 args)
+{
+    if (!anim_pool.editing_animation)
+    {
+        verify(false, "there is no animation to end. Please specify the editing animation with beginAnimation()", __LINE__);
+    }
+
+    anim_pool.editing_animation->args = args;
+
+    // get the animation ready to start play
+    if (anim_pool.editing_animation->framecount > 0)
+    {
+        anim_pool.editing_animation->time = anim_pool.editing_animation->timeframes[0];
+    }
+
+
+    anim_pool.editing_animation = NULL;
+}
+void updateAnimation(Animation *anim)
+{
+    if (anim->framecount <= 0)
+        return;
+
+    anim->time -= tick_speed;
+    if (anim->time <= 0)
+    {
+        printf("%d %d\n", anim->time, anim->timeframes[0]);
+        ++anim->frame;
+        if (anim->frame >= anim->framecount && anim->args == 0)
+        {
+            anim->time = 0;
+            anim->frame = anim->framecount - 1;
+            return;
+        }
+        // loops (make this less hardcoded pls, like a switch statement for args or turn it into a bitflag)
+        if (anim->frame >= anim->framecount && anim->args != 0)
+        {
+            anim->time = anim->timeframes[0];
+            anim->frame = 0;
+            return;
+        }
+        anim->time = anim->timeframes[anim->frame];
+    }
+}
+
+void imageDrawAnimated(Image *img, int x, int y, Animation *anim)
+{
+
+    SDL_Rect scale_rect = {x * IMG_PIXEL_SIZE, y * IMG_PIXEL_SIZE, IMG_PIXEL_SIZE, IMG_PIXEL_SIZE};
+    img->clip_rect = (SDL_Rect){.x = anim->xframes[anim->frame] * IMG_PIXEL_SIZE,
+                                .y = anim->yframes[anim->frame] * IMG_PIXEL_SIZE,
+                                .w = IMG_PIXEL_SIZE, .h = IMG_PIXEL_SIZE};
+    SDL_RenderCopy(app.renderer, img->img, &img->clip_rect, &scale_rect);
+}
+
 // make a drawmulti function that can draw a larger texture clip rect (still locked to 16 pixel values)
 // make the ui map with the title screen and buttons
 // make a simple button thing that runs a function on press
@@ -24,8 +164,6 @@
 //      an init function where it sets up music/sprites/etc, and a close function where it 'frees' all the resources that it
 //      was using. There will be a screen for menu, settings (if possible), win, lose, classic mode setup, classic mode, and
 //      one screen for each puzzle mode level.
-
-// animations are going to be so hard fuck
 
 Application app;
 ImagePool img_pool;
@@ -184,6 +322,11 @@ typedef struct
 
     Image *boardImg;
     Image *snakeImg;
+    Animation *snakeHeadNorthAnim;
+    Animation *snakeHeadSouthAnim;
+    Animation *snakeHeadWestAnim;
+    Animation *snakeHeadEastAnim;
+    // Animation *snakeBodyAnim;
     Image *uiImg;
 
     Music *lowMus;
@@ -250,6 +393,29 @@ void gameInit()
     game.snake.move_timer = MOVE_TIMER_RESET;
     putSnake(BOARD_WIDTH / 2, BOARD_HEIGHT / 2);
     makeBoard(10, 10);
+
+    game.snakeHeadNorthAnim = AnimationAlloc();
+    game.snakeHeadSouthAnim = AnimationAlloc();
+    game.snakeHeadWestAnim = AnimationAlloc();
+    game.snakeHeadEastAnim = AnimationAlloc();
+    // game.snakeBodyAnim = AnimationAlloc();
+
+    beginAnimation(game.snakeHeadNorthAnim);
+    addFrame(0, 0, 8000);
+    addFrame(1, 0, 8000);
+    endAnimation(1);
+    beginAnimation(game.snakeHeadSouthAnim);
+    addFrame(7, 1, 8000);
+    addFrame(8, 1, 8000);
+    endAnimation(1);
+    beginAnimation(game.snakeHeadWestAnim);
+    addFrame(1, 3, 8000);
+    addFrame(2, 3, 8000);
+    endAnimation(1);
+    beginAnimation(game.snakeHeadEastAnim);
+    addFrame(7, 0, 8000);
+    addFrame(8, 0, 8000);
+    endAnimation(1);
 
     spawnFood(5);
 }
@@ -331,7 +497,25 @@ void drawSnake()
             printf("hello\n");
         }
         if (i == 0)
-            imageDraw(game.snakeImg, game.snake.parts[i].x, game.snake.parts[i].y, 2, 2);
+        {
+            switch(game.snake.next_move_direction)
+            {
+            case NORTH:
+                imageDrawAnimated(game.snakeImg, game.snake.parts[i].x, game.snake.parts[i].y, game.snakeHeadNorthAnim);
+                break;
+            case SOUTH:
+                imageDrawAnimated(game.snakeImg, game.snake.parts[i].x, game.snake.parts[i].y, game.snakeHeadSouthAnim);
+                break;
+            case WEST:
+                imageDrawAnimated(game.snakeImg, game.snake.parts[i].x, game.snake.parts[i].y, game.snakeHeadWestAnim);
+                break;
+            case EAST:
+                imageDrawAnimated(game.snakeImg, game.snake.parts[i].x, game.snake.parts[i].y, game.snakeHeadEastAnim);
+                break;
+            default:
+                break;
+            }
+        }
         else if (i == game.snake.length - 1)
             imageDraw(game.snakeImg, game.snake.parts[i].x, game.snake.parts[i].y, 0, 2);
         else
@@ -520,13 +704,17 @@ void game_loop()
         // menu here
         break;
     case GAME_CLASSIC:
-
         while (!game_caught_up)
         {
             gameRules();
             updateFood();
             musicFadeIn(game.lowMus);
             musicFadeIn(game.highMus);
+            updateAnimation(game.snakeHeadNorthAnim);
+            updateAnimation(game.snakeHeadSouthAnim);
+            updateAnimation(game.snakeHeadWestAnim);
+            updateAnimation(game.snakeHeadEastAnim);
+
             game_caught_up = gameCaughtUp();
         }
 
